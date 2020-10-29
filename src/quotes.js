12ed2e53ffbbcabc0db061b366701323d3e29765
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2020-10-29 19:40:15 +0200

    Update quotes.js

--- src/quotes.js
+++ src/quotes.js
@@ -2,4 +2,22 @@
 
 module.exports = [
   "Ek soek minder wannabee's en meer mense met 'n missie!",
+  "Almal is ryk van die liefde en ek’s alweer die armste"
+  "Want al wat rerig in die lewe tel is hoe jy lyk"
+  "Wie se Afrikaans is dood?"
+  "Dis net musiek wat my beter laat voel!"
+  "Die vingers van my gevoelens krap diep in my verstand!"
+  "Dit het alles begin een aand by een of ander baar!"
+  "Ons hou vas aanmekaar, maar niemand gaan ooit joune wees nie!"
+  "Die lewe is hard en ek verstanie meer die punt nie!"
+  "Jack Parow baby mind the gap the myth the legend the mind behind the rap!"
+  "Die taalkundiges bespreek, en bespigel steeds my woord"
+  "Ek het al baie ver gewen en baie ver verloor."
+  "Jack Parow skiet lyrics soos ‘n kop skoot!”
+  "Laat ons suip, laat ons suip dat die donkerte verdwyn, want hiers ek nou ek’s hier vir jou, ek’s anie kant anie brand met ŉ bottel in my hand”
+  "Dis skemer in my woorde en middernag in my vers, ek dans saam spoke om die flikers van 'n kers"
+  "Ons ondergange wat werd is vat 'n tydjie om in te stap"
+  "Ek voel soos 'n Hero as jy langs my staan! Ag sweetheart ons behoort mos saam!"
+  "I was born in this beautiful country, born & fkn bred. I’ve bled, I’ve cried, I’ve laughed, I’ve kots'd in the grass & the dust and now it’s time to fkn stand up for what we    believe in"
+
 ]
